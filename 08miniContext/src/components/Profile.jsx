00935814245132e2b73e3e91a63dda173addc32f
saurabh wanjari <141421981+wanjarisaurabh@users.noreply.github.com>
2025-03-11 15:05:26 +0000

    y

--- 08miniContext/src/components/Profile.jsx
+++ 08miniContext/src/components/Profile.jsx
@@ -6,7 +6,7 @@ function Profile() {
     
     if (!user) return <div>please login</div>
 
-    return <div>Welcome {user.username}</div>
+    return <><div>Welcome {user.username}</div></> 
 }
 
 export default Profile
